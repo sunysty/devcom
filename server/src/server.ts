f9c397c84556804a0f5d479e94d3fd59b6b9863e
sunysty <seokjiseon@gmail.com>
2023-02-01 19:04:50 +0900

    chore: docker file 추가

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,6 +10,6 @@ app.use(morgan('dev')); //로그관리 개발타입
 app.get('/', (_, res) => res.send('running')); //요청을 받으면 응답으로 "running"을 보내줌
 
 let port = 4000;
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`http://localhost:${port}`);
 }); //app의 해당포트로 접속하면 해당코드를 실행
