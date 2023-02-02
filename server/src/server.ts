083072a78084f6787cdc34b1901c3152b7c5e932
sunysty <seokjiseon@gmail.com>
2023-02-02 10:16:58 +0900

    feat: server에 db연동하는 작업

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import { AppDataSource } from './data-source';
 
 const app = express();
 
@@ -9,7 +10,15 @@ app.use(morgan('dev')); //로그관리 개발타입
 
 app.get('/', (_, res) => res.send('running')); //요청을 받으면 응답으로 "running"을 보내줌
 
+//app의 해당포트로 접속하면 해당코드를 실행
 let port = 4000;
 app.listen(port, async () => {
   console.log(`http://localhost:${port}`);
-}); //app의 해당포트로 접속하면 해당코드를 실행
+
+  //db와 server연결하는 코드블럭
+  AppDataSource.initialize()
+    .then(() => {
+      console.log('database initialize!');
+    })
+    .catch((error) => console.log(error));
+});
